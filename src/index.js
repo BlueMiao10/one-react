0ddce23d8473c7b91e333892ff0e012ca2b2b173
BlueYang <2931442305@qq.com>
2022-02-12 15:52:25 +0800

    文件分模块

--- src/index.js
+++ src/index.js
@@ -1,46 +1,5 @@
-const React = {
-  createElement(tag,attrs,...children) {
-    return {
-      tag,
-      attrs,
-      children
-    }
-  }
-};
-function setAttribute(node, attrs) {
-  if(!attrs) return;
-
-  for(let key in attrs) {
-    if(key.startsWith('on')) {
-      node[key.toLocaleLowerCase()] = attrs[key];
-    } else if(key === 'style') {
-      Object.assign(node.style, attrs[key]);
-    } else {
-      node[key] = attrs[key];
-    }
-  }
-}
-
-
-function render(vDom,container){
-  let node
-  if(typeof vDom === 'string'){
-    node = document.createTextNode(vDom)
-  }
-  if (typeof vDom === 'object'){
-    node = document.createElement(vDom.tag)
-    setAttribute(node,vDom.attrs)
-    vDom.children.forEach(childVDom => render(childVDom,node))
-  }
-  container.appendChild(node)
-}
-
-const ReactDom = {
-  render(vDom, container) {
-    container.innerHTML = '';
-    render(vDom, container);
-  }
-}
+import React from "./lib/react";
+import ReactDom from "./lib/react-dom";
 
 let styleObj = {
   color:'blue',
---
+++ src/lib/react-dom.js
@@ -0,0 +1,36 @@
+function setAttribute(node, attrs) {
+  if(!attrs) return;
+
+  for(let key in attrs) {
+    if(key.startsWith('on')) {
+      node[key.toLocaleLowerCase()] = attrs[key];
+    } else if(key === 'style') {
+      Object.assign(node.style, attrs[key]);
+    } else {
+      node[key] = attrs[key];
+    }
+  }
+}
+
+
+function render(vDom,container){
+  let node
+  if(typeof vDom === 'string'){
+    node = document.createTextNode(vDom)
+  }
+  if (typeof vDom === 'object'){
+    node = document.createElement(vDom.tag)
+    setAttribute(node,vDom.attrs)
+    vDom.children.forEach(childVDom => render(childVDom,node))
+  }
+  container.appendChild(node)
+}
+
+const ReactDom = {
+  render(vDom, container) {
+    container.innerHTML = '';
+    render(vDom, container);
+  }
+}
+
+export default ReactDom
