86344dfa37de5e6ba61101906355ab46b5a1d576
BlueYang <2931442305@qq.com>
2022-02-12 15:32:23 +0800

    渲染标签

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const React = {
   createElement(tag,attrs,...children) {
-    return{
+    return {
       tag,
       attrs,
       children
@@ -8,7 +8,30 @@ const React = {
   }
 };
 
-let div = (<div className="header">hello world
-  <span>sha</span>
+function render(vDom,container){
+  let node
+  if(typeof vDom === 'string'){
+    node = document.createTextNode(vDom)
+  }
+  if (typeof vDom === 'object'){
+    node = document.createElement(vDom.tag)
+    vDom.children.forEach(childVDom => render(childVDom,node))
+  }
+  container.appendChild(node)
+}
+
+const ReactDom = {
+  render(vDom, container) {
+    container.innerHTML = '';
+    render(vDom, container);
+  }
+}
+
+let div = (<div className="header">
+  <span>hello world</span>
+  <br/>
+  <span>一起向未来</span>
 </div>);
   console.log(div)
+
+ReactDom.render(div,document.body)
